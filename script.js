2150e18e37eafdcb1d3105941f8d220d22947bc3
jasmina8835 <sunnatovajasmina8002@gmail.com>
2024-02-16 19:48:57 +0500

    new

---
+++ script.js
@@ -0,0 +1,54 @@
+// if (15 < 2) {
+//     console.log('Salom');
+// }else {
+//     console.log('Hayr');
+// }
+
+// TERNAR
+
+// (15 > 2) ? console.log('Salom') : console.log('Hayr');
+
+// SWITCH CASE
+// let year = 2024
+// switch (year) {
+//     case 2024:
+//         console.log('Joriy yil');
+//         break;
+//     case 1991:
+//         console.log('Mustaqillik');
+//         break;
+//     case 1992:
+//         console.log('Konstitutsiya');
+//         break;
+//     default:
+//         console.log('Boshqa yil');
+//         break;
+// }
+
+
+
+let m = Number(prompt('Qancha masofa otildi'))
+
+if (m <= 55) {
+    console.log('Toshkent');
+}else if (m > 55 && m <= 175) {
+    console.log('Sirdaryo');
+}else if (m <= 325 && m > 175){
+    console.log('Jizzax');
+}else if (m > 325 && m <= 356){
+    console.log('Samarqand');
+}else {
+    console.log('Siz uydasiz');
+}
+
+let a = Number(prompt('Raqamni kiriting'))
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
